3358c03163a30524650e8de69b55689f7b5766d3
Adi Satria <adisatria@apps.ipb.ac.id>
2024-05-23 23:30:25 +0700

    initial commit

---
+++ src/server/routes.js
@@ -0,0 +1,23 @@
+const { postPredictHandler, getAllPredictHandler } = require("./handler");
+
+const routes = [
+  {
+    path: "/predict",
+    method: "POST",
+    handler: postPredictHandler,
+    options: {
+      payload: {
+        allow: "multipart/form-data",
+        multipart: true,
+        maxBytes: 1000000,
+      },
+    },
+  },
+  {
+    path: "/predict/histories",
+    method: "GET",
+    handler: getAllPredictHandler,
+  },
+];
+
+module.exports = routes;
